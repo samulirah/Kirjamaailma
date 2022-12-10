1877334939ef148c9807afc1e845237ef52ff123
samulirah <101732050+samulirah@users.noreply.github.com>
2022-12-10 16:32:33 +0200

    alustava ostoskori

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import uuid from 'react-uuid';
 
+//aja koodi: npm i react-uuid
 
-export default function Order({cart, removeFromCart, updateAmount}) {
+export default function Order({cart, removeFromCart}, updateAmount) {
+  let sum = 0;
 
   return (
-    <div>Order</div>
+    <div>
+      <h3 className="header">Ostoskori</h3>
+      <table className="table">
+        <tbody>
+          {cart.map(product => {
+            sum+=parseFloat(product.hinta);
+            return (
+              <tr key={uuid()}>
+                <td>{product.tuotenimi}</td>
+                <td>{product.hinta} €</td>
+                <td><a href="#" onClick={() => removeFromCart(product.tuotenro)}>Poista tuote ostoskorista</a></td>
+              </tr>
+            )
+            })}
+              <tr key={uuid()}>
+            <td></td>
+            <td>{sum.toFixed(2)} €</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
-} 
+}
